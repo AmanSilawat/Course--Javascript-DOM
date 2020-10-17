11f24b48515ee12aac66912c04e4a22471f2c36d
aman <aman.khan.silawat.aks@gmail.com>
2020-10-17 14:55:58 +0530

    setAttribute Exercise

---
+++ 02-set-attr.js
@@ -0,0 +1,7 @@
+document.getElementById('myInput').setAttribute('value', 'Enter Name');
+
+const myElement = document.createElement('div');
+
+myElement.setAttribute('class', 'dcode');
+
+document.body.appendChild(myElement);
